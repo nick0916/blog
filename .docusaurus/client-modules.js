d2e7bca3846b7f50d3d3f0e26ed6e45944bf4d21
nick0916 <nickliao2013@gmail.com>
2022-04-04 15:33:13 +0800

    test

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/nickliao/Documents/my-github/blog/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/nickliao/Documents/my-github/blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/nickliao/Documents/my-github/blog/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/nickliao/Documents/my-github/blog/src/css/custom.css"),
+];
--- deploy-github.js
+++ deploy-github.js
@@ -3,8 +3,8 @@ const ghpages = require('gh-pages');
 ghpages.publish(
   'build',
   {
-    branch: 'docusaurus-page',
-    repo: 'https://github.com/nick0916/blog.git',
+        branch: 'docusaurus-page',
+        repo: 'https://github.com/nick0916/blog.git'
   },
   (error) => {
       if (error){
